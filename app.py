01992f8424e8430fdb75551a3e4ca44226206d3d
PengYu <chen2011fen@gmail.com>
2021-04-08 23:32:17 +0800

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	app.py
    #	requirements.txt

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
+    MessageEvent, TextMessage, TextSendMessage, StickerSendMessage,
 )
 
 app = Flask(__name__)
@@ -38,11 +38,25 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    s = event.message.text
-    ss = "shit"
+    msg = event.message.text
+    s = "什麼?"
+
+    if "貼圖" in msg:
+        sticker_message = StickerSendMessage(
+            package_id='1',
+            sticker_id='1'
+        )
+        line_bot_api.reply_message(
+            event.reply_token,
+            sticker_message)
+        return
+
+    if msg in ["hi", "Hi"]:
+        s = "嗨"
+
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=s+ss))
+        TextSendMessage(text=s))
 
 
 if __name__ == "__main__":
