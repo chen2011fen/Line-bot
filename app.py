4faa60b29b7ae3660f1b1e17f2654098ec980f7d
PengYu <chen2011fen@gmail.com>
2021-04-08 23:39:02 +0800

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	app.py
    #	requirements.txt

--- app.py
+++ app.py
@@ -43,8 +43,8 @@ def handle_message(event):
 
     if "貼圖" in msg:
         sticker_message = StickerSendMessage(
-            package_id='1',
-            sticker_id='1'
+            package_id='2',
+            sticker_id='23'
         )
         line_bot_api.reply_message(
             event.reply_token,
